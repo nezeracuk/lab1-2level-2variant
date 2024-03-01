da61e1edbf0f4b4d56cbc93757cc0150f7f4a5f4
Nazarii <skibickignazar@gmail.com>
2024-03-01 10:17:53 +0200

    updated-lab

--- Second_Lab.py
+++ Second_Lab.py
@@ -1,9 +1,16 @@
 def monotony(array):
-    tedium = array[1] >= array[0]
-    for i in range(2, len(array)):
-        if (array[i] >= array[i - 1]) != tedium:
+    increases = 0
+    decreases = 0
+    if len(array) == 0:
+        return True
+    else:
+        for i in range(len(array) - 1):
+            if array[i + 1] >= array[i]:
+                increases += 1
+        for i in range(len(array) - 1):
+            if array[i + 1] <= array[i]:
+                decreases += 1
+        if increases == len(array) - 1 or decreases == len(array) - 1:
+            return True
+        else:
             return False
-    return True
-
-
-
--- test_second_lab.py
+++ test_second_lab.py
@@ -6,7 +6,8 @@ class Test_Second_Lab(unittest.TestCase):
         self.assertEqual(monotony([3,5,6,7,3,2,5,7,5]), False)
         self.assertEqual(monotony([1,2,3,4,5]),True)
         self.assertEqual(monotony([5,5,5,5,5,5]),True)
-        self.assertEqual(monotony([5,4,3,2,1]),True)
+        self.assertEqual(monotony([]),True)
+
 
 if __name__ == '__main__':
     unittest.main()
