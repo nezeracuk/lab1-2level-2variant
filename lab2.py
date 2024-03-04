bc28c0cffa9373af7066c087a6f9f03304950248
Nazarii Skibytskyi <151003673+nezeracuk@users.noreply.github.com>
2024-03-04 13:14:47 +0200

    lab2.py

--- lab2.py
+++ lab2.py
@@ -1,13 +1,10 @@
 def min_board_size(N, W, H):
-    def is_enough_space(size):
-        return (size // W) * (size // H) >= N
-
     smallest_num, largest_num = 1, max(W, H) * N
     min_size = largest_num
 
     while smallest_num <= largest_num:
         mid = (smallest_num + largest_num) // 2
-        if is_enough_space(mid):
+        if (mid // W) * (mid // H) >= N:
             min_size = mid
             largest_num = mid - 1
         else:
