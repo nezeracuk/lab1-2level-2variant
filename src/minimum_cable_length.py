e9e4b1b8e98bbecc35442646e94bccffeaa178ce
Nazarii <skibickignazar@gmail.com>
2024-05-03 11:28:56 +0300

    added lab8 and folder resources

---
+++ src/minimum_cable_length.py
@@ -0,0 +1,94 @@
+class Graph:
+    def __init__(self, nodes=None):
+        """
+        Ініціалізація графа.
+        Args:
+        nodes (list, optional): Список вузлів для додавання у граф спочатку.
+        """
+        self.edges = []
+        self.nodes = set(nodes) if nodes else set()
+
+    def add_edge(self, start, end, weight):
+        """
+        Додає ребро до графа з вказаними вагою і вузлами.
+
+        Args:
+        start (str): Початковий вузол ребра.
+        end (str): Кінцевий вузол ребра.
+        weight (int): Вага ребра.
+        """
+        self.edges.append((weight, start, end))
+        self.nodes.update([start, end])
+
+    def find_parent(self, parent, node):
+        """
+        Рекурсивний пошук кореневого вузла для даного вузла.
+
+        Args:
+        parent (dict): Словник батьківських відносин між вузлами.
+        node (str): Вузол, для якого потрібно знайти кореневий вузол.
+
+        Returns:
+        Кореневий вузол для заданого вузла.
+        """
+        if parent[node] == node:
+            return node
+        return self.find_parent(parent, parent[node])
+
+    def kruskal_mst(self):
+        """
+        Реалізація алгоритму Крускала для знаходження мінімального остовного дерева графа.
+
+        Returns:
+        Вага кістякового дерева або -1, якщо створення MST неможливе.
+        """
+        if not self.edges:
+            return -1
+
+        #Сортуємо ребра за вагою для подальшої обробки у порядку зростання.
+        self.edges.sort()
+
+        #Ініціалізація батьків кожного вузла, початково вузол є батьком самому собі.
+        parent = {node: node for node in self.nodes}
+
+        #Ініціалізуємо загальну вагу остовного дерева та лічильник включених ребер.
+        total_weight = 0
+        mst_edges = 0
+
+        #Обробка кожного ребра за порядком ваги.
+        for weight, start, end in self.edges:
+            #Знаходимо кореневі вузли для поточних вузлів ребра.
+            root_start = self.find_parent(parent, start)
+            root_end = self.find_parent(parent, end)
+
+            #Якщо кореневі вузли різні, ребро не утворює циклу.
+            if root_start != root_end:
+                total_weight += weight  #Додаємо вагу ребра до загальної ваги MST.
+                mst_edges += 1  #Збільшуємо кількість ребер у MST.
+                parent[root_start] = root_end  #Об'єднуємо дві компоненти зв'язності.
+
+        #Перевіряємо, чи всі вузли з'єднані. Якщо так, повертаємо загальну вагу, якщо ні - повертаємо -1.
+        return total_weight if mst_edges == len(self.nodes) - 1 else -1
+
+        #Перевірка, чи всі вузли з'єднані
+        if mst_edges == len(self.nodes) - 1:
+            return total_weight
+        else:
+            return -1
+
+def read_input_file(min_cable_path):
+    """
+    Зчитування даних ребер з файлу CSV.
+
+    Args:
+    min_cable_len (str): Шлях до файлу CSV.
+
+    Returns:
+    Початковий вузол, кінцевий вузол, вага.
+    """
+    edges = []
+    with open(min_cable_path, "r") as file:
+        for line in file:
+            parts = line.split(',')
+            edges.append((parts[0], parts[1], int(parts[2])))
+    return edges
---
+++ test/Test_minimum_cable_length.py
@@ -0,0 +1,62 @@
+import unittest
+from src.minimum_cable_length import Graph, read_input_file
+
+class TestGraphFunctionality(unittest.TestCase):
+    def load_graph_from_file(self, file_path):
+        """
+        Бере файл CSV, який містить дані про ребра та повертає граф
+
+        Юзаємо read_input_file,щоб прочитати дані з csv файлу,
+        ініціалізує граф з вузлами, отриманими з цих ребер, та додає ребра до графа.
+
+        file_path = Шлях до CSV файлу, який містить дані про ребра.
+        Формат файлу повинен бути start/end/weight, де
+        start та end = вузли, weight= вага,
+        в нашому випадку відстань між колодязями.
+        """
+        edges = read_input_file(file_path)
+        nodes = set()
+        for start, end, weight in edges:
+            nodes.update([start, end])
+        graph = Graph(nodes=list(nodes))
+        for start, end, weight in edges:
+            graph.add_edge(start, end, weight)
+        return graph
+
+    def test_default_case(self):
+        """
+        Тестування звичайного випадку мст
+        :return:
+        """
+        graph = self.load_graph_from_file('../resources/communication_wells3.csv')
+        expected_mst_weight = 150
+        result = graph.kruskal_mst()
+        self.assertEqual(result, expected_mst_weight)
+
+    def test_no_edges(self):
+        """
+        Тестування відсутності ребер у графі
+        """
+        graph = self.load_graph_from_file('../resources/communication_wells2.csv')
+        result = graph.kruskal_mst()
+        self.assertEqual(result, -1)
+
+    def test_isolated_nodes(self):
+        """
+        Тестування наявності незалежних вершин, для цього створюю окремо
+        isolated_edges незалежно від інших еджів та додаю умову відокремлених
+        вершин, які незалежні від початкової умови.
+        """
+        isolated_edges = read_input_file('../resources/communication_wells4.csv')
+        isolated_nodes = set()
+        for start, end, weight in isolated_edges:
+            isolated_nodes.update([start, end])
+        isolated_nodes.update(['K4', 'K5'])
+        isolated_graph = Graph(nodes=list(isolated_nodes))
+        for start, end, weight in isolated_edges:
+            isolated_graph.add_edge(start, end, weight)
+        result = isolated_graph.kruskal_mst()
+        self.assertEqual(result, -1)
+
+if __name__ == '__main__':
+    unittest.main()
