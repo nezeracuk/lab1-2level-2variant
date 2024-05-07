e17652c5e66b06e1064207eb1c9139d9dc814806
Nazarii Skibytskyi <151003673+nezeracuk@users.noreply.github.com>
2024-05-07 16:43:20 +0300

    Updated path to resources folder

--- test/Test_minimum_cable_length.py
+++ test/Test_minimum_cable_length.py
@@ -27,7 +27,7 @@ class TestGraphFunctionality(unittest.TestCase):
         Тестування звичайного випадку мст
         :return:
         """
-        graph = self.load_graph_from_file('../resources/communication_wells3.csv')
+        graph = self.load_graph_from_file('../src/resources/communication_wells3.csv')
         expected_mst_weight = 150
         result = graph.kruskal_mst()
         self.assertEqual(result, expected_mst_weight)
@@ -36,7 +36,7 @@ class TestGraphFunctionality(unittest.TestCase):
         """
         Тестування відсутності ребер у графі
         """
-        graph = self.load_graph_from_file('../resources/communication_wells2.csv')
+        graph = self.load_graph_from_file('../src/resources/communication_wells2.csv')
         result = graph.kruskal_mst()
         self.assertEqual(result, -1)
 
@@ -46,7 +46,7 @@ class TestGraphFunctionality(unittest.TestCase):
         isolated_edges незалежно від інших еджів та додаю умову відокремлених
         вершин, які незалежні від початкової умови.
         """
-        isolated_edges = read_input_file('../resources/communication_wells4.csv')
+        isolated_edges = read_input_file('../src/resources/communication_wells4.csv')
         isolated_nodes = set(['K4', 'K5'])
         isolated_graph = Graph(nodes=list(isolated_nodes))
         for start, end, weight in isolated_edges:
